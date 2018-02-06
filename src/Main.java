2012d60be4406b302022464feeb3f224b15d01b9
EndaMoore <endamoore@gmail.com>
2018-02-06 20:55:38 +0000

    added loop as test

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World! ");
         System.out.println("Hello whats New");
+
+        int i = 1, j = 10;
+        do
+        {
+            if(i++ > --j) /* Line 4 */
+            {
+                continue;
+            }
+        } while (i < 5);
+        System.out.println("i = " + i + " and j = " + j); /* Line 9 */
     }
 }
 // Comment add as a test 18/01/2017

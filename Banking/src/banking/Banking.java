a1f2f771c6fc2cf4b0137759c7288f88863ac856
EndaMoore <endamoore@gmail.com>
2018-02-10 16:35:32 +0000

    Test banking project

---
+++ Banking/src/banking/Banking.java
@@ -0,0 +1,13 @@
+package banking;
+
+/**
+ * @author endam
+ */
+public class Banking {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Banking test class ");
+    }
+    
+}
